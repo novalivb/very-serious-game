a2bbfe1865324a456eb34c0418783fe1d57ab52d
novalivb <novalivb@gmail.com>
2026-06-22 20:49:51 -0700

    THEME : Homey Bee
    Added main character: the Homey Bee!
    Homey Bee has just returned to the hive from a long, long aaa journey and is so tired! He just wants to take a nap.  His wings are SO sore! Luckily, he lives at the top of an endless hive...

--- godot/very-serious-game/source/levels/level-objects/boulder.gd
+++ godot/very-serious-game/source/levels/level-objects/boulder.gd
@@ -4,5 +4,5 @@ extends RigidBody2D
 func _ready() -> void:
 	angular_velocity = randf_range(-1, 1)
 	linear_velocity.x = randf_range(0, 200)
-	if position.x > 0:
+	if position.x > 0: # always point toward world center (level center) NOT CAMERA CENTER
 		linear_velocity.x *= -1
